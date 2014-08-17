7738ce88a06dee6a6c29c40e6a92edac384d8b8d
adasilva <awesomeashley527@gmail.com>
2014-08-17 11:02:03 -0500

    water costs file

---
+++ waterCosts.py
@@ -0,0 +1,19 @@
+
+class waterCosts:
+    # Costs per gallon in different cities:
+
+    # Average person uses 100 gallons of water per day
+    # (http://www.epa.gov/WaterSense/pubs/indoor.html; accessed Aug 2014)    # The average household (family of 4) uses 12,000 gallons per month
+
+    # Austin charges $9.95 per 1000 gallons for 11,001 - 20,000 total
+    # gallons of usage
+    # (http://www.austintexas.gov/department/austin-water-utility-service-rates; accessed Aug 2014)
+    self.austin = 9.95/1000.
+
+    # San Antonio charges $0.1529 per 100 gallons for 12,717 - total 
+    # gallons of usage 
+    self.sanAntonio = 0.1529/100.
+
+    self.dallas = 0
+
+    self.houston = 0
